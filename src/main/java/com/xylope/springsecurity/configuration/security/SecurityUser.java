dd7343b11a279197f641081b57642391f26e91c9
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-08-03 16:04:56 +0900

    UserDetailsService 및 UserDetails 구현 Spring Security 관련 package 작성

---
+++ src/main/java/com/xylope/springsecurity/configuration/security/SecurityUser.java
@@ -0,0 +1,41 @@
+package com.xylope.springsecurity.configuration.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@RequiredArgsConstructor
+@Getter
+public class SecurityUser implements UserDetails {
+    private final String username;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
